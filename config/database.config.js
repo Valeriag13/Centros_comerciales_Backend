1d4d0b5b8fe29c53070b1d7c6498c5deb05d1e74
Valeriag13 <vgiraldo@umanizales.edu.co>
2023-07-09 21:36:05 -0500

    Cambio de url de conexion a la base de datos

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: 'mongodb+srv://Valeria:Valeriag13@atlascluster.k0nvq7i.mongodb.net/Centros_comerciales'
+    url: 'mongodb://localhost:27017/Centros_comerciales'
+    //mongodb+srv://Valeria:Valeriag13@atlascluster.k0nvq7i.mongodb.net/Centros_comerciales
 }
